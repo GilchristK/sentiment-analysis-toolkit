13c3bc18f534370a20bc761639587f71871f1581
GilchristK (Katuta G. Kaunda) <katutakaunda@gmail.com>
2020-10-21 14:36:37 +0200

    added method to read excel file from data_store

--- sentiment_analyser.py
+++ sentiment_analyser.py
@@ -1,4 +1,21 @@
 #Author: Katuta Kaunda
 #Date: 20-oct-2020
 #Description: social media sentiment analysis using the AFINN lexicon
+import sys,getopt
+import os
+import pandas as pd
 
+
+
+
+#Given a path load excel document into pandas dataframe
+def read_excel(path):
+    df = pd.read_excel(path)
+    print(df)
+
+def main():
+    
+    read_excel(r'data_store\reviews_pseudoanonymised.xlsx')
+
+if __name__ == '__main__':
+    main()
