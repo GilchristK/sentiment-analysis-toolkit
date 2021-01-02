0fddca3f7f92e45f1a0298b09046897312265dd3
GilchristK (Katuta G. Kaunda) <katutakaunda@gmail.com>
2021-01-02 07:11:40 +0200

    added labels and colors to the bar chart

--- sentiment_visualiser.py
+++ sentiment_visualiser.py
@@ -1,11 +1,17 @@
 #Author: Katuta Kaunda
 #Date: 20-oct-2020
 #Description: social media sentiment visualiser
+import sys,getopt
+import os
+import pandas as pd
 import plotly.express as plt
 from sentiment_analyser import *
 def generateVisualization(df):
-    df2 = df.groupby(by=['sentiment'])
-    fig = plt.bar(df, x='sentiment', y='score',title='AFINN sentiment Analysis')
+    #df2 = df.groupby(by=['sentiment'])
+    #fig = plt.bar(df, x='sentiment', y='score',title='AFINN sentiment Analysis')
+    #fig.show()
+    df1 = df.groupby(["sentiment"]).count().reset_index()
+    fig = plt.bar(df1,y=df.groupby(["sentiment"]).size(),x="sentiment",color='sentiment',title='AFINN sentiment Analysis') 
     fig.show()
 
 def main():
